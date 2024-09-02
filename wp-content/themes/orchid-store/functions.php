0416de202450c68c40d0ec866fbe516a219cd94e
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2024-09-02 22:45:45 +0700

    update code

--- wp-content/themes/orchid-store/functions.php
+++ wp-content/themes/orchid-store/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Orchid Store functions and definitions
  *
@@ -7,11 +8,11 @@
  * @package Orchid_Store
  */
 
-$current_theme = wp_get_theme( 'orchid-store' );
+$current_theme = wp_get_theme('orchid-store');
 
-define( 'ORCHID_STORE_VERSION', $current_theme->get( 'Version' ) );
+define('ORCHID_STORE_VERSION', $current_theme->get('Version'));
 
-if ( ! function_exists( 'orchid_store_setup' ) ) {
+if (!function_exists('orchid_store_setup')) {
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -19,17 +20,18 @@ if ( ! function_exists( 'orchid_store_setup' ) ) {
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function orchid_store_setup() {
+	function orchid_store_setup()
+	{
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
 		 * If you're building a theme based on Orchid Store, use a find and replace
 		 * to change 'orchid-store' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'orchid-store', get_template_directory() . '/languages' );
+		load_theme_textdomain('orchid-store', get_template_directory() . '/languages');
 
 		// Add default posts and comments RSS feed links to head.
-		add_theme_support( 'automatic-feed-links' );
+		add_theme_support('automatic-feed-links');
 
 		/*
 		 * Let WordPress manage the document title.
@@ -37,23 +39,23 @@ if ( ! function_exists( 'orchid_store_setup' ) ) {
 		 * hard-coded <title> tag in the document head, and expect WordPress to
 		 * provide it for us.
 		 */
-		add_theme_support( 'title-tag' );
+		add_theme_support('title-tag');
 
 		/*
 		 * Enable support for Post Thumbnails on posts and pages.
 		 *
 		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		 */
-		add_theme_support( 'post-thumbnails' );
-		add_image_size( 'orchid-store-thumbnail-extra-large', 800, 600, true );
-		add_image_size( 'orchid-store-thumbnail-large', 800, 450, true );
+		add_theme_support('post-thumbnails');
+		add_image_size('orchid-store-thumbnail-extra-large', 800, 600, true);
+		add_image_size('orchid-store-thumbnail-large', 800, 450, true);
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus(
 			array(
-				'menu-1' => esc_html__( 'Primary Menu', 'orchid-store' ),
-				'menu-2' => esc_html__( 'Secondary Menu', 'orchid-store' ),
-				'menu-3' => esc_html__( 'Top Header Menu', 'orchid-store' ),
+				'menu-1' => esc_html__('Primary Menu', 'orchid-store'),
+				'menu-2' => esc_html__('Secondary Menu', 'orchid-store'),
+				'menu-3' => esc_html__('Top Header Menu', 'orchid-store'),
 			)
 		);
 
@@ -85,7 +87,7 @@ if ( ! function_exists( 'orchid_store_setup' ) ) {
 		);
 
 		// Add theme support for selective refresh for widgets.
-		add_theme_support( 'customize-selective-refresh-widgets' );
+		add_theme_support('customize-selective-refresh-widgets');
 
 		/**
 		 * Add support for core custom logo.
@@ -107,10 +109,10 @@ if ( ! function_exists( 'orchid_store_setup' ) ) {
 		 *
 		 * @link https://make.wordpress.org/core/2021/06/29/block-based-widgets-editor-in-wordpress-5-8/
 		 */
-		remove_theme_support( 'widgets-block-editor' );
+		remove_theme_support('widgets-block-editor');
 	}
 
-	add_action( 'after_setup_theme', 'orchid_store_setup' );
+	add_action('after_setup_theme', 'orchid_store_setup');
 }
 
 /**
@@ -120,19 +122,21 @@ if ( ! function_exists( 'orchid_store_setup' ) ) {
  *
  * @global int $content_width
  */
-function orchid_store_content_width() {
+function orchid_store_content_width()
+{
 	// This variable is intended to be overruled from themes.
 	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
-	$GLOBALS['content_width'] = apply_filters( 'orchid_store_content_width', 640 );
+	$GLOBALS['content_width'] = apply_filters('orchid_store_content_width', 640);
 }
-add_action( 'after_setup_theme', 'orchid_store_content_width', 0 );
+add_action('after_setup_theme', 'orchid_store_content_width', 0);
 
 
 /**
  * Enqueue scripts and styles.
  */
-function orchid_store_scripts() {
+function orchid_store_scripts()
+{
 
 	wp_enqueue_style(
 		'orchid-store-style',
@@ -166,7 +170,7 @@ function orchid_store_scripts() {
 		'all'
 	);
 
-	if ( is_rtl() ) {
+	if (is_rtl()) {
 
 		wp_enqueue_style(
 			'orchid-store-main-style-rtl',
@@ -199,73 +203,74 @@ function orchid_store_scripts() {
 	wp_register_script(
 		'orchid-store-bundle',
 		get_template_directory_uri() . '/assets/dist/js/bundle.min.js',
-		array( 'jquery' ),
+		array('jquery'),
 		ORCHID_STORE_VERSION,
 		true
 	);
 
 	$script_obj = array(
-		'ajax_url'              => esc_url( admin_url( 'admin-ajax.php' ) ),
-		'homeUrl'               => esc_url( home_url() ),
+		'ajax_url'              => esc_url(admin_url('admin-ajax.php')),
+		'homeUrl'               => esc_url(home_url()),
 		'isUserLoggedIn'        => is_user_logged_in(),
-		'isCartMessagesEnabled' => orchid_store_get_option( 'enable_cart_messages' ),
+		'isCartMessagesEnabled' => orchid_store_get_option('enable_cart_messages'),
 	);
 
-	$script_obj['scroll_top'] = orchid_store_get_option( 'display_scroll_top_button' );
+	$script_obj['scroll_top'] = orchid_store_get_option('display_scroll_top_button');
 
-	if ( class_exists( 'WooCommerce' ) ) {
+	if (class_exists('WooCommerce')) {
 
-		if ( get_theme_mod( 'orchid_store_field_product_added_to_cart_message', esc_html__( 'Product successfully added to cart!', 'orchid-store' ) ) ) {
+		if (get_theme_mod('orchid_store_field_product_added_to_cart_message', esc_html__('Product successfully added to cart!', 'orchid-store'))) {
 
-			$script_obj['added_to_cart_message'] = get_theme_mod( 'orchid_store_field_product_added_to_cart_message', esc_html__( 'Product successfully added to cart!', 'orchid-store' ) );
+			$script_obj['added_to_cart_message'] = get_theme_mod('orchid_store_field_product_added_to_cart_message', esc_html__('Product successfully added to cart!', 'orchid-store'));
 		}
 
-		if ( get_theme_mod( 'orchid_store_field_product_removed_from_cart_message', esc_html__( 'Product has been removed from your cart!', 'orchid-store' ) ) ) {
+		if (get_theme_mod('orchid_store_field_product_removed_from_cart_message', esc_html__('Product has been removed from your cart!', 'orchid-store'))) {
 
-			$script_obj['removed_from_cart_message'] = get_theme_mod( 'orchid_store_field_product_removed_from_cart_message', esc_html__( 'Product has been removed from your cart!', 'orchid-store' ) );
+			$script_obj['removed_from_cart_message'] = get_theme_mod('orchid_store_field_product_removed_from_cart_message', esc_html__('Product has been removed from your cart!', 'orchid-store'));
 		}
 
-		if ( get_theme_mod( 'orchid_store_field_cart_update_message', esc_html__( 'Cart items has been updated successfully!', 'orchid-store' ) ) ) {
+		if (get_theme_mod('orchid_store_field_cart_update_message', esc_html__('Cart items has been updated successfully!', 'orchid-store'))) {
 
-			$script_obj['cart_updated_message'] = get_theme_mod( 'orchid_store_field_cart_update_message', esc_html__( 'Cart items has been updated successfully!', 'orchid-store' ) );
+			$script_obj['cart_updated_message'] = get_theme_mod('orchid_store_field_cart_update_message', esc_html__('Cart items has been updated successfully!', 'orchid-store'));
 		}
 
-		if ( get_theme_mod( 'orchid_store_field_product_cols_in_mobile', 1 ) ) {
-			$script_obj['product_cols_on_mobile'] = get_theme_mod( 'orchid_store_field_product_cols_in_mobile', 1 );
+		if (get_theme_mod('orchid_store_field_product_cols_in_mobile', 1)) {
+			$script_obj['product_cols_on_mobile'] = get_theme_mod('orchid_store_field_product_cols_in_mobile', 1);
 		}
 
-		if ( get_theme_mod( 'orchid_store_field_display_plus_minus_btns', true ) ) {
-			$script_obj['displayPlusMinusBtns'] = get_theme_mod( 'orchid_store_field_display_plus_minus_btns', true );
+		if (get_theme_mod('orchid_store_field_display_plus_minus_btns', true)) {
+			$script_obj['displayPlusMinusBtns'] = get_theme_mod('orchid_store_field_display_plus_minus_btns', true);
 		}
 
-		if ( get_theme_mod( 'orchid_store_field_cart_display', 'default' ) ) {
-			$script_obj['cartDisplay'] = ( class_exists( 'Addonify_Floating_Cart' ) ) ? apply_filters( 'orchid_store_cart_display_filter', get_theme_mod( 'orchid_store_field_cart_display', 'default' ) ) : 'default';
+		if (get_theme_mod('orchid_store_field_cart_display', 'default')) {
+			$script_obj['cartDisplay'] = (class_exists('Addonify_Floating_Cart')) ? apply_filters('orchid_store_cart_display_filter', get_theme_mod('orchid_store_field_cart_display', 'default')) : 'default';
 		}
 
-		if ( class_exists( 'Addonify_Wishlist' ) ) {
-			$script_obj['addToWishlistText']     = get_option( 'addonify_wishlist_btn_label', 'Add to wishlist' );
-			$script_obj['addedToWishlistText']   = get_option( 'addonify_wishlist_btn_label_when_added_to_wishlist', 'Added to wishlist' );
-			$script_obj['alreadyInWishlistText'] = get_option( 'addonify_wishlist_btn_label_if_added_to_wishlist', 'Already in wishlist' );
+		if (class_exists('Addonify_Wishlist')) {
+			$script_obj['addToWishlistText']     = get_option('addonify_wishlist_btn_label', 'Add to wishlist');
+			$script_obj['addedToWishlistText']   = get_option('addonify_wishlist_btn_label_when_added_to_wishlist', 'Added to wishlist');
+			$script_obj['alreadyInWishlistText'] = get_option('addonify_wishlist_btn_label_if_added_to_wishlist', 'Already in wishlist');
 		}
 	}
 
-	wp_localize_script( 'orchid-store-bundle', 'orchid_store_obj', $script_obj );
+	wp_localize_script('orchid-store-bundle', 'orchid_store_obj', $script_obj);
 
-	wp_enqueue_script( 'orchid-store-bundle' );
+	wp_enqueue_script('orchid-store-bundle');
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'orchid_store_scripts' );
+add_action('wp_enqueue_scripts', 'orchid_store_scripts');
 
 
 /**
  * Enqueue scripts and styles for admin.
  */
-function orchid_store_admin_enqueue() {
+function orchid_store_admin_enqueue()
+{
 
-	wp_enqueue_script( 'media-upload' );
+	wp_enqueue_script('media-upload');
 
 	wp_enqueue_media();
 
@@ -280,13 +285,13 @@ function orchid_store_admin_enqueue() {
 	wp_enqueue_script(
 		'orchid-store-admin-script',
 		get_template_directory_uri() . '/admin/js/admin-script.js',
-		array( 'jquery' ),
+		array('jquery'),
 		ORCHID_STORE_VERSION,
 		true
 	);
 }
-add_action( 'admin_enqueue_scripts', 'orchid_store_admin_enqueue' );
-add_action( 'wp_ajax_wp_ajax_install_plugin', 'wp_ajax_install_plugin' );
+add_action('admin_enqueue_scripts', 'orchid_store_admin_enqueue');
+add_action('wp_ajax_wp_ajax_install_plugin', 'wp_ajax_install_plugin');
 
 
 
@@ -297,37 +302,106 @@ add_action( 'wp_ajax_wp_ajax_install_plugin', 'wp_ajax_install_plugin' );
  *
  * @since 1.0.0
  */
-function orchid_store_activate_plugin() {
+function orchid_store_activate_plugin()
+{
 
 	$return_data = array(
 		'success' => false,
 		'message' => '',
 	);
 
-	if ( isset( $_POST['_ajax_nonce'] ) && ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['_ajax_nonce'] ) ), 'updates' ) ) {
-		$return_data['message'] = esc_html__( 'Invalid security token.', 'orchid-store' );
-		wp_send_json( $return_data );
+	if (isset($_POST['_ajax_nonce']) && !wp_verify_nonce(sanitize_text_field(wp_unslash($_POST['_ajax_nonce'])), 'updates')) {
+		$return_data['message'] = esc_html__('Invalid security token.', 'orchid-store');
+		wp_send_json($return_data);
 	}
 
-	$activation = activate_plugin( WP_PLUGIN_DIR . '/addonify-floating-cart/addonify-floating-cart.php' );
+	$activation = activate_plugin(WP_PLUGIN_DIR . '/addonify-floating-cart/addonify-floating-cart.php');
 
-	if ( ! is_wp_error( $activation ) ) {
+	if (!is_wp_error($activation)) {
 
 		$return_data['success'] = true;
-		$return_data['message'] = esc_html__( 'The plugin is activated successfully.', 'orchid-store' );
+		$return_data['message'] = esc_html__('The plugin is activated successfully.', 'orchid-store');
 	} else {
 
 		$return_data['message'] = $activation->get_error_message();
 	}
 
-	wp_send_json( $return_data );
+	wp_send_json($return_data);
 }
-add_action( 'wp_ajax_orchid_store_activate_plugin', 'orchid_store_activate_plugin' );
+add_action('wp_ajax_orchid_store_activate_plugin', 'orchid_store_activate_plugin');
+
+/**
+ * Product default attributes
+ */
+add_action('woocommerce_before_single_product_summary', function () {
+	global $product;
+
+	if (!$product instanceof WC_Product_Variable) return;
+
+	$checkCountAttribute = count($product->get_attributes()) === count($product->get_default_attributes());
+
+	if (!empty($product->get_default_attributes())) {
+		$checkDefaultAttributeExit = false;
+		$arrayDefaultAttributes = [];
+
+		foreach ($product->get_default_attributes() as $key => $value) {
+			if (ctype_digit($value)) {
+				$checkDefaultAttributeExit = true;
+			} else {
+				$arrayDefaultAttributes[$key] = $value;
+			}
+		}
+
+		$checkCountAttribute = count($product->get_attributes()) === count($arrayDefaultAttributes);
+
+		if ($checkDefaultAttributeExit || !$checkCountAttribute) {
+			$product->set_default_attributes($arrayDefaultAttributes);
+			$checkCountAttribute = count($product->get_attributes()) === count($product->get_default_attributes());
+		}
+	}
+
+	if (!empty($product->get_attributes()) && !$checkCountAttribute) {
+
+		$default_attribute = [];
+
+		if (!empty($product->get_default_attributes())) {
+			$default_attribute = $product->get_default_attributes();
+		}
+
+		/** @var WC_Product_Attribute $attribute */
+		foreach ($product->get_attributes() as $key => $attribute) {
+			$options = $attribute->get_options();
+
+			if (!array_key_exists($attribute['name'], $default_attribute)) {
+				$arrayCheck = [];
+				foreach ($options as $option) {
+					$tag_slug = get_term($option, $key)->slug;
+					array_push($arrayCheck, $tag_slug);
+				}
+
+				if ($key == "pa_size") {
+					if (in_array("small", $arrayCheck)) {
+						$default_attribute[$key] = "small";
+					} else {
+						$default_attribute[$key] = get_term($options[0], $key)->slug;;
+					}
+				} else {
+					sort($arrayCheck);
+					$default_attribute[$key] = $arrayCheck[0];
+				}
+			}
+		}
+
+		$product->set_default_attributes($default_attribute);
+
+		update_post_meta($product->get_id(), '_default_attributes', $default_attribute);
+	}
+});
 
 
-if ( defined( 'ELEMENTOR_VERSION' ) ) {
+if (defined('ELEMENTOR_VERSION')) {
 
-	add_action( 'elementor/editor/before_enqueue_scripts', 'orchid_store_admin_enqueue' );
+	add_action('elementor/editor/before_enqueue_scripts', 'orchid_store_admin_enqueue');
 }
 
 
@@ -354,7 +428,7 @@ require get_template_directory() . '/customizer/customizer.php';
 /**
  * Load WooCommerce compatibility file.
  */
-if ( class_exists( 'WooCommerce' ) ) {
+if (class_exists('WooCommerce')) {
 
 	require get_template_directory() . '/inc/woocommerce.php';
 
